b5b1a7251c151ab114b8d4c13e6d761679f0bc85
tsionawit <your_email@example.com>
2026-02-27 22:12:06 +0300

    Update UI

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -20,12 +20,30 @@ const updateUI = () => {
         const li = document.createElement('li');
         li.className = 'list-item';
         li.innerHTML = `
-            
-            <span>${item}</span>
+            <div class="item-info">
+                <input type="checkbox" class="check" ${item.checked ? 'checked' : ''}>
+                <span class="${item.checked ? 'completed' : ''}">${item.name}</span>
+            </div>
             <button onclick="deleteItem(${index})" style="background:none; border:none; color:red; cursor:pointer;">
                 <i class="fa-regular fa-trash-can"></i>
             </button>
         `;
+
+        const checkBox = li.querySelector('.check');
+        const textSpan = li.querySelector('span');
+
+        checkBox.addEventListener('change', function() {
+            if (this.checked) { 
+                
+                textSpan.classList.add('completed');
+                items[index].checked = true; 
+            } else {
+                textSpan.classList.remove('completed');
+                items[index].checked = false; 
+            }
+        });
+
+
         shoppingList.appendChild(li);
     });
 };
@@ -49,3 +67,4 @@ addBtn.addEventListener('click', addItem);
 itemNameInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') addItem();
 });
+
